d5375e9ddde20cd62b8057f68112fc166f382e0b
Tyler Sommer <sommertm@gmail.com>
2013-08-11 20:42:22 -0700

    Added license blurb to all source files

--- Tests/Factory/FactoryRegistryTest.php
+++ Tests/Factory/FactoryRegistryTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OrkestraPdfBundle package.
+ *
+ * Copyright (c) Orkestra Community
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Orkestra\Bundle\PdfBundle\Tests\Factory;
 
 use Orkestra\Bundle\PdfBundle\Factory\FactoryRegistry;
