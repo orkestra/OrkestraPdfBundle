48fd41c06f104c01cc3a042fa4066f7ac0476f60
Tyler Sommer <sommertm@gmail.com>
2013-08-11 19:04:38 -0700

    WkPdfFactory now create()s a WkPdfBuilder

--- Factory/WkPdfFactory.php
+++ Factory/WkPdfFactory.php
@@ -3,20 +3,24 @@
 namespace Orkestra\Bundle\PdfBundle\Factory;
 
 use Orkestra\Bundle\PdfBundle\Pdf\WkPdf;
+use Orkestra\Bundle\PdfBundle\Pdf\WkPdfBuilder;
+use Symfony\Component\Finder\Finder;
 use TCPDF;
 use ReflectionClass;
 
 class WkPdfFactory implements PdfFactoryInterface
 {
+    private $executable = 'wkhtmltopdf';
+
     /**
      * Creates a new WkPdf
      *
      * @param array $options
      *
-     * @return \Orkestra\Bundle\PdfBundle\Pdf\PdfInterface|\Orkestra\Bundle\PdfBundle\Pdf\WkPdf
+     * @return \Orkestra\Bundle\PdfBundle\Pdf\PdfInterface|\Orkestra\Bundle\PdfBundle\Pdf\WkPdfBuilder
      */
     public function create(array $options = array())
     {
-        return new WkPdf();
+        return new WkPdfBuilder($this->executable, $options);
     }
 }
